e1bc1004db0cd41cfa551ba69b72201bd45e45c2
Rafael Guimarães <rafaelsanct@gmail.com>
2020-03-29 01:02:21 -0300

    init project

---
+++ src/Routes.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+
+import { isAuthenticated } from "./services/auth";
+
+import StartRequestPage from "./pages/StartRequestPage";
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+    <Route
+        {...rest}
+        render={props =>
+            isAuthenticated() ? (
+                <Component {...props} />
+            ) : (
+                    <Redirect to={{ pathname: "/login", state: { from: props.location } }} />
+                )
+        }
+    />
+);
+
+const Routes = () => (
+    <BrowserRouter>
+        <Switch>
+            <Route exact path="/" component={StartRequestPage} />
+            {/* <Route exact path="/login" component={Login} />
+      <PrivateRoute exact path="/" component={HomePage} />
+      <PrivateRoute exact path="/cart" component={CartPage} />
+      <PrivateRoute exact path="/cart/find" component={FindItem} />
+      <PrivateRoute exact path="/cart/add" component={NewCart} />
+      <PrivateRoute exact path="/cart/history" component={HistoryCart} />
+      <PrivateRoute exact path="/cart/all" component={AllCart} />
+      <PrivateRoute exact path="/clientes" component={ClientesPage} />
+      <PrivateRoute exact path="/stock" component={StockPage} /> */}
+            {/* <Route path="/signup" component={() => <h1>SignUp</h1>} /> */}
+            <Route path="*" component={() => <h1>Page not found</h1>} />
+        </Switch>
+    </BrowserRouter>
+);
+
+export default Routes;
